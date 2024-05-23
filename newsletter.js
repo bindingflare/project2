347bdae87873b16ced98d6fba75583192b7e1b53
junhyun bang <flintintoe@gmail.com>
2024-05-23 17:41:21 +0900

    Move js scripts to directory files. - Add "mapapi.js" - Add "newsletter.js"

---
+++ newsletter.js
@@ -0,0 +1,34 @@
+
+// Creates popup for form submit
+function createSubmitPopout(event) {
+    console.log("creating popup!");
+
+    const cover = document.getElementById("popup-cover")
+    cover.style.display = "block";
+
+    const popup = document.getElementById("popup-subscribed");
+    window.setTimeout(function(){
+        popup.style.transform = 'scale(1)';
+    },0);
+
+    event.preventDefault();
+}
+
+// Add event listener to form submit
+const form = document.getElementById("subscribe");
+form.addEventListener("submit", createSubmitPopout);
+
+// Hide popup when close is pressed
+function closePopup(event) {
+    console.log("closing popup!");
+
+    const cover = document.getElementById("popup-cover")
+
+    const popup = document.getElementById("popup-subscribed");
+    popup.style.transform = 'scale(0)';
+
+    window.setTimeout(function(){
+        cover.style.display = "none";
+        cover.style.display = 'none';
+    },700);
+}
